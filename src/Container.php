faa57ccca17041056bbcc2ed2973b5e617d50ad1
aurelzefi <aurel.zefi@eurostep.it>
2020-06-25 16:44:28 +0200

    wip

--- src/Container.php
+++ src/Container.php
@@ -5,13 +5,33 @@ namespace Aurel\Container;
 use Closure;
 use Exception;
 use ReflectionClass;
+use ReflectionMethod;
+use ReflectionParameter;
 
 class Container
 {
+    /**
+     * The bindings in the container.
+     *
+     * @var array
+     */
     public $bindings = [];
 
+    /**
+     * The singleton bindings in the container.
+     *
+     * @var array
+     */
     protected $singletons = [];
 
+    /**
+     * Register a binding in the container.
+     *
+     * @param  \Closure|string  $abstract
+     * @param  mixed  $concrete
+     * @param  bool  $singleton
+     * @return void
+     */
     public function bind($abstract, $concrete = null, $singleton = false)
     {
         if (is_null($concrete)) {
@@ -21,11 +41,27 @@ class Container
         $this->bindings[$abstract] = compact('concrete', 'singleton');
     }
 
+    /**
+     * Register a singleton binding in the container.
+     *
+     * @param  \Closure|string  $abstract
+     * @param  mixed  $concrete
+     * @return void
+     */
     public function singleton($abstract, $concrete = null)
     {
         $this->bind($abstract, $concrete, true);
     }
 
+    /**
+     * Get a binding from the container.
+     *
+     * @param  string  $abstract
+     * @param  array  $parameters
+     * @return mixed
+     *
+     * @throws \Exception
+     */
     public function get($abstract, array $parameters = [])
     {
         if (isset($this->singletons[$abstract])) {
@@ -34,7 +70,7 @@ class Container
 
         $concrete = $this->getConcrete($abstract);
 
-        if ($this->isBuildable($concrete, $abstract)) {
+        if ($this->isBuildable($abstract, $concrete)) {
             $object = $this->build($concrete, $parameters);
         } else {
             $object = $this->get($concrete, $parameters);
@@ -47,6 +83,12 @@ class Container
         return $object;
     }
 
+    /**
+     * Get the concrete binding for the given abstract.
+     *
+     * @param  string  $abstract
+     * @return mixed
+     */
     protected function getConcrete($abstract)
     {
         if (isset($this->bindings[$abstract])) {
@@ -56,11 +98,26 @@ class Container
         return $abstract;
     }
 
-    protected function isBuildable($concrete, $abstract)
+    /**
+     * Determine if the binding is buildable.
+     * @param  string  $abstract
+     * @param  mixed  $concrete
+     * @return bool
+     */
+    protected function isBuildable($abstract, $concrete)
     {
-        return $concrete === $abstract || $concrete instanceof Closure;
+        return $abstract === $concrete || $concrete instanceof Closure;
     }
 
+    /**
+     * Build the given concrete.
+     *
+     * @param  mixed  $concrete
+     * @param  array  $parameters
+     * @return mixed|object
+     *
+     * @throws \ReflectionException
+     */
     public function build($concrete, $parameters)
     {
         if ($concrete instanceof Closure) {
@@ -68,10 +125,6 @@ class Container
         }
 
         $reflector = new ReflectionClass($concrete);
-
-        if (! $reflector->isInstantiable()) {
-            throw new Exception();
-        }
 
         $constructor = $reflector->getConstructor();
 
@@ -81,24 +134,87 @@ class Container
 
         $dependencies = $constructor->getParameters();
 
-        $instances = $this->resolveDependencies($dependencies);
+        $instances = $this->resolveDependencies($dependencies, $parameters);
 
         return $reflector->newInstanceArgs($instances);
     }
 
-    protected function resolveDependencies(array $dependencies)
+    /**
+     * Resolve the given dependencies.
+     *
+     * @param  array  $dependencies
+     * @param  array  $parameters
+     * @return array
+     *
+     * @throws \ReflectionException
+     */
+    protected function resolveDependencies(array $dependencies, $parameters)
     {
         $results = [];
 
         foreach ($dependencies as $dependency) {
+            if (array_key_exists($dependency->name, $parameters)) {
+                $results[] = $parameters[$dependency->name];
 
+                continue;
+            }
+
+            $class = $dependency->getClass()->name;
+
+            $results[] = is_null($class) ? $this->resolvePrimitive($dependency) : $this->get($class);
         }
 
         return $results;
     }
 
+    /**
+     * Resolve the given primitive parameter.
+     *
+     * @param  \ReflectionParameter  $parameter
+     * @return mixed
+     *
+     * @throws \ReflectionException
+     */
+    protected function resolvePrimitive(ReflectionParameter $parameter)
+    {
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+    }
+
+    /**
+     * Determine if the binding is a singleton.
+     *
+     * @param  string  $abstract
+     * @return bool
+     */
     protected function isSingleton($abstract)
     {
-        return $this->bindings[$abstract]['singleton'];
+        return isset($this->bindings[$abstract]['singleton']) && $this->bindings[$abstract]['singleton'] === true;
+    }
+
+    /**
+     * Call a method from the container.
+     *
+     * @param  object|string $object
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     *
+     * @throws \ReflectionException
+     */
+    public function call($object, $method, array $parameters = [])
+    {
+        if (is_string($object)) {
+            $object = $this->get($object);
+        }
+
+        $reflector = new ReflectionMethod($object, $method);
+
+        $dependencies = $reflector->getParameters();
+
+        $instances = $this->resolveDependencies($dependencies, $parameters);
+
+        return call_user_func_array([$object, $method], $instances);
     }
 }
